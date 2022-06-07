51bf357bb70d65a1d5980a216a2c434d104e808b
Hil_mitamaam <ahmadhilmitamaam15@gmail.com>
2022-06-07 16:27:11 +0700

    Initial commit

---
+++ app/src/main/java/fragment/AllNewsFragment.kt
@@ -0,0 +1,34 @@
+package fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import data.DataNews
+import adapter.NewsAdapter
+import com.idn.newsapp.bindNewsHeadline
+import com.idn.newsapp.databinding.FragmentAllNewsBinding
+
+class AllNewsFragment : Fragment() {
+
+    private lateinit var binding: FragmentAllNewsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        binding = FragmentAllNewsBinding.inflate(inflater, container, false)
+        binding.rvAllNews.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = NewsAdapter(DataNews.dataAllNews)
+        }
+
+        bindNewsHeadline(binding.newsHeadline, 0)
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/fragment/TopFragment.kt
@@ -0,0 +1,34 @@
+package fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import data.DataNews
+import adapter.NewsAdapter
+import com.idn.newsapp.bindNewsHeadline
+import com.idn.newsapp.databinding.FragmentTopBinding
+
+class TopFragment : Fragment() {
+
+    private lateinit var binding: FragmentTopBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        binding = FragmentTopBinding.inflate(inflater, container, false)
+        binding.rvTopNews.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = NewsAdapter(DataNews.dataTopNews)
+        }
+
+        bindNewsHeadline(binding.newsHeadline, 2)
+
+        return binding.root
+    }
+}
